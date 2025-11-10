a0a3e3a26f51fd9cf260d11b29f2dea202b29ee2
mahmoudkassem712 <mahmoud.kassem@mozare3.net>
2025-11-10 16:21:52 +0200

    add site list

--- examples/Wurd/Controllers/CustomizeApiController.cs
+++ examples/Wurd/Controllers/CustomizeApiController.cs
@@ -23,13 +23,11 @@ namespace Wurd.Controllers
     public class CustomizeApiController : ControllerBase
     {
         private readonly IApi _api;
-        private readonly PageService _service;
 
 
-        public CustomizeApiController(IApi api, PageService service)
+        public CustomizeApiController(IApi api)
         {
             _api = api;
-            _service = service;
         }
 
         // GET: api/dua/slug/adeya
@@ -44,12 +42,78 @@ namespace Wurd.Controllers
             return Ok(page);
         }
 
+
         [Route("sites-list")]
         [HttpGet]
-        [Authorize]
-        public async Task<PageListModel> List()
+        public async Task<IActionResult> SiteList()
         {
-            var model = await _service.GetList();
+            var model = new PageListModel
+            {
+                Sites = (await _api.Sites.GetAllAsync())
+                      .OrderByDescending(s => s.IsDefault)
+                      .Select(s => new PageListModel.PageSite
+                      {
+                          Id = s.Id,
+                          Title = s.Title,
+                          Slug = "/",
+                          EditUrl = "manager/site/edit/"
+                      }).ToList(),
+            };
+
+            foreach (var site in model.Sites)
+            {
+                site.Pages.AddRange(await GetPageStructure(site.Id));
+            }
+
+            return Ok(model);
+
+        }
+
+        private async Task<List<PageListModel.PageItem>> GetPageStructure(Guid siteId)
+        {
+            var pages = new List<PageListModel.PageItem>();
+
+            // Get the configured expanded levels
+            var expandedLevels = 0;
+            using (var config = new Config(_api))
+            {
+                expandedLevels = config.ManagerExpandedSitemapLevels;
+            }
+
+            // Get the sitemap and transform
+            var sitemap = await _api.Sites.GetSitemapAsync(siteId, false);
+            var drafts = await _api.Pages.GetAllDraftsAsync(siteId);
+            foreach (var item in sitemap)
+            {
+                pages.Add(MapRecursive(siteId, item, 0, expandedLevels, drafts));
+            }
+            return pages;
+        }
+
+        private PageListModel.PageItem MapRecursive(Guid siteId, SitemapItem item, int level, int expandedLevels, IEnumerable<Guid> drafts)
+        {
+            var model = new PageListModel.PageItem
+            {
+                Id = item.Id,
+                SiteId = siteId,
+                Title = item.MenuTitle,
+                TypeName = item.PageTypeName,
+                Published = item.Published.HasValue ? item.Published.Value.ToString("yyyy-MM-dd") : null,
+                Status = drafts.Contains(item.Id) ? PageListModel.PageItem.Draft :
+                    !item.Published.HasValue ? PageListModel.PageItem.Unpublished : "",
+                EditUrl = "manager/page/edit/",
+                IsExpanded = level < expandedLevels,
+                IsCopy = item.OriginalPageId.HasValue,
+                IsRestricted = item.Permissions.Count > 0,
+                IsScheduled = item.Published.HasValue && item.Published.Value > DateTime.Now,
+                IsUnpublished = !item.Published.HasValue,
+                Permalink = item.Permalink
+            };
+
+            foreach (var child in item.Items)
+            {
+                model.Items.Add(MapRecursive(siteId, child, level + 1, expandedLevels, drafts));
+            }
             return model;
         }
 
@@ -74,5 +138,38 @@ namespace Wurd.Controllers
             return Ok("hello");
 
         }
+
+        //[Route("sites-list")]
+        //[HttpGet]
+        //public async Task<IActionResult> List()
+        //{
+        //    var sites = await _api.Sites.GetAllAsync();
+        //    var pages = new List<PageListModel.PageItem>();
+
+        //    foreach (var site in sites)
+        //    {
+        //        var sitePages = await _api.Pages.GetAllAsync(site.Id);
+        //        pages.AddRange(sitePages.Select(p => new PageListModel.PageItem
+        //        {
+        //            Id = p.Id,
+        //            Title = p.Title,
+        //            TypeName = p.TypeId,
+        //            SiteTitle = site.Title,
+        //            Permalink = p.Permalink
+        //        }));
+        //    }
+
+        //    var model = new PageListModel
+        //    {
+        //        Sites = sites.Select(s => new PageListModel.SiteItem
+        //        {
+        //            Id = s.Id,
+        //            Title = s.Title
+        //        }).ToList(),
+        //        Pages = pages
+        //    };
+
+        //    return Ok(model);
+        //}
     }
 }
