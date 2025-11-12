d1f01734df7753f37b79de882f7562c7f34bdb97
mahmoudkassem712 <mahmoud.kassem@mozare3.net>
2025-11-12 10:48:29 +0200

    handle customize api controller

--- examples/Wurd/Controllers/CustomizeApiController.cs
+++ examples/Wurd/Controllers/CustomizeApiController.cs
@@ -22,14 +22,15 @@ namespace Wurd.Controllers
     public class CustomizeApiController : ControllerBase
     {
         private readonly IApi _api;
+        private readonly PageService _service;
 
 
-        public CustomizeApiController(IApi api)
+        public CustomizeApiController(IApi api, PageService service)
         {
             _api = api;
+            _service = service;
         }
 
-        // GET: api/dua/slug/adeya
         [HttpGet("slug/{slug}")]
         public async Task<IActionResult> GetBySlug(string slug)
         {
@@ -45,76 +46,9 @@ namespace Wurd.Controllers
         [HttpGet]
         public async Task<IActionResult> SiteList()
         {
-            var model = new PageListModel
-            {
-                Sites = (await _api.Sites.GetAllAsync())
-                      .OrderByDescending(s => s.IsDefault)
-                      .Select(s => new PageListModel.PageSite
-                      {
-                          Id = s.Id,
-                          Title = s.Title,
-                          Slug = "/",
-                          EditUrl = "manager/site/edit/"
-                      }).ToList(),
-            };
-
-            foreach (var site in model.Sites)
-            {
-                site.Pages.AddRange(await GetPageStructure(site.Id));
-            }
+            var model = await _service.GetList();
 
             return Ok(model);
-
-        }
-
-
-        private async Task<List<PageListModel.PageItem>> GetPageStructure(Guid siteId)
-        {
-            var pages = new List<PageListModel.PageItem>();
-
-            // Get the configured expanded levels
-            var expandedLevels = 0;
-            using (var config = new Config(_api))
-            {
-                expandedLevels = config.ManagerExpandedSitemapLevels;
-            }
-
-            // Get the sitemap and transform
-            var sitemap = await _api.Sites.GetSitemapAsync(siteId, false);
-            var drafts = await _api.Pages.GetAllDraftsAsync(siteId);
-            foreach (var item in sitemap)
-            {
-                pages.Add(MapRecursive(siteId, item, 0, expandedLevels, drafts));
-            }
-            return pages;
-        }
-
-        private PageListModel.PageItem MapRecursive(Guid siteId, SitemapItem item, int level, int expandedLevels, IEnumerable<Guid> drafts)
-        {
-
-            var model = new PageListModel.PageItem
-            {
-                Id = item.Id,
-                SiteId = siteId,
-                Title = item.MenuTitle,
-                TypeName = item.PageTypeName,
-                Published = item.Published.HasValue ? item.Published.Value.ToString("yyyy-MM-dd") : null,
-                Status = drafts.Contains(item.Id) ? PageListModel.PageItem.Draft :
-                    !item.Published.HasValue ? PageListModel.PageItem.Unpublished : "",
-                EditUrl = "manager/page/edit/",
-                IsExpanded = level < expandedLevels,
-                IsCopy = item.OriginalPageId.HasValue,
-                IsRestricted = item.Permissions.Count > 0,
-                IsScheduled = item.Published.HasValue && item.Published.Value > DateTime.Now,
-                IsUnpublished = !item.Published.HasValue,
-                Permalink = item.Permalink
-            };
-
-            foreach (var child in item.Items)
-            {
-                model.Items.Add(MapRecursive(siteId, child, level + 1, expandedLevels, drafts));
-            }
-            return model;
         }
 
         [HttpGet]
@@ -137,14 +71,6 @@ namespace Wurd.Controllers
             return Ok(result);
 
         }
-        [HttpGet]
-        [Route("print-hello")]
-        public virtual async Task<IActionResult> print()
-        {
-
-            return Ok("hello");
-
-        }
 
         [Route("child-pages-list")]
         [HttpGet]
@@ -158,5 +84,97 @@ namespace Wurd.Controllers
 
             return Ok(result);
         }
+
+
+        [HttpGet]
+        [Route("ping")]
+        public virtual async Task<IActionResult> ping()
+        {
+            return Ok("Welcome to wurd cms");
+        }
+
+
+        ///////////////////////////////////////////////
+
+        //[Route("sites-list")]
+        //[HttpGet]
+        //public async Task<IActionResult> SiteList()
+        //{
+        //    var model = await _service.GetList();
+
+        //    return Ok(model);
+
+        //    //var model = new PageListModel
+        //    //{
+        //    //    Sites = (await _api.Sites.GetAllAsync())
+        //    //          .OrderByDescending(s => s.IsDefault)
+        //    //          .Select(s => new PageListModel.PageSite
+        //    //          {
+        //    //              Id = s.Id,
+        //    //              Title = s.Title,
+        //    //              Slug = "/",
+        //    //              EditUrl = "manager/site/edit/"
+        //    //          }).ToList(),
+        //    //};
+
+        //    //foreach (var site in model.Sites)
+        //    //{
+        //    //    site.Pages.AddRange(await GetPageStructure(site.Id));
+        //    //}
+
+        //    //return Ok(model);
+
+        //}
+
+
+        //private async Task<List<PageListModel.PageItem>> GetPageStructure(Guid siteId)
+        //{
+        //    var pages = new List<PageListModel.PageItem>();
+
+        //    // Get the configured expanded levels
+        //    var expandedLevels = 0;
+        //    using (var config = new Config(_api))
+        //    {
+        //        expandedLevels = config.ManagerExpandedSitemapLevels;
+        //    }
+
+        //    // Get the sitemap and transform
+        //    var sitemap = await _api.Sites.GetSitemapAsync(siteId, false);
+        //    var drafts = await _api.Pages.GetAllDraftsAsync(siteId);
+        //    foreach (var item in sitemap)
+        //    {
+        //        pages.Add(MapRecursive(siteId, item, 0, expandedLevels, drafts));
+        //    }
+        //    return pages;
+        //}
+
+        //private PageListModel.PageItem MapRecursive(Guid siteId, SitemapItem item, int level, int expandedLevels, IEnumerable<Guid> drafts)
+        //{
+
+        //    var model = new PageListModel.PageItem
+        //    {
+        //        Id = item.Id,
+        //        SiteId = siteId,
+        //        Title = item.MenuTitle,
+        //        TypeName = item.PageTypeName,
+        //        Published = item.Published.HasValue ? item.Published.Value.ToString("yyyy-MM-dd") : null,
+        //        Status = drafts.Contains(item.Id) ? PageListModel.PageItem.Draft :
+        //            !item.Published.HasValue ? PageListModel.PageItem.Unpublished : "",
+        //        EditUrl = "manager/page/edit/",
+        //        IsExpanded = level < expandedLevels,
+        //        IsCopy = item.OriginalPageId.HasValue,
+        //        IsRestricted = item.Permissions.Count > 0,
+        //        IsScheduled = item.Published.HasValue && item.Published.Value > DateTime.Now,
+        //        IsUnpublished = !item.Published.HasValue,
+        //        Permalink = item.Permalink
+        //    };
+
+        //    foreach (var child in item.Items)
+        //    {
+        //        model.Items.Add(MapRecursive(siteId, child, level + 1, expandedLevels, drafts));
+        //    }
+        //    return model;
+        //}
+
     }
 }
